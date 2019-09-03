10f6fbfa58416988dde63c2cad1713ba5ef82d25
c-roiger <charlotte.roiger@gmail.com>
2019-09-03 09:02:48 -0500

    finish up get_mask_hough still need to test

--- R/get_mask_hough.R
+++ R/get_mask_hough.R
@@ -1,13 +1,13 @@
 #' @examples
 #'
 #' x3p <- read_x3p("")
-#' grooves <-get_grooves_hough(x3p_to_df(x3p))
+#' grooves <-get_grooves_hough(x3p_to_df(x3p), qu = 0.99)
 #'
 #' get_mask_hough(x3p, grooves)
 #'
 get_mask_hough <- function(land.x3p, grooves){
-  left <- grooves$left.groove.fit(1:ncol(land.x3p$surface.matrix))
-  right <- grooves$right.groove.fit(1:ncol(land.x3p$surface.matrix))
+  left <- grooves$left.groove.fit(1:ncol(land.x3p$surface.matrix))/x3p_get_scale(land.x3p)
+  right <- grooves$right.groove.fit(1:ncol(land.x3p$surface.matrix))/x3p_get_scale(land.x3p)
 
 
   if(is.null(land.x3p$mask)){
@@ -17,15 +17,17 @@ get_mask_hough <- function(land.x3p, grooves){
 
   mask <- raster::as.matrix(land.x3p$mask)
   mask <- sapply(1:length(left), FUN = function(i){
-    mask[ i,1:floor(left[i])] <- 1
+    mask[ i,1:floor(left[i])] <- "#c4221a"
     mask[i,]
-  })
+  }) %>%t()
 
+  mask <- sapply(1:length(right), FUN = function(i){
+      mask[i, floor(right[i]):ncol(mask)] <- "#3279a8"
+      mask[i,]
+  }) %>% t()
 
-  land.x3p <- land.x3p %>% x3p_add_mask_layer(mask, color = "red", annotation = "Left groove")
+  land.x3p <- land.x3p %>% x3p_add_mask(mask = mask)
 
-
-  browser()
 
 }
 
