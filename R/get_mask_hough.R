ba917fe399db01c72d4b464d9a81502638340734
heike <hofmann@iastate.edu>
2019-09-03 18:16:38 -0500

    included data and fixed some dependencies

--- R/get_mask_hough.R
+++ R/get_mask_hough.R
@@ -1,22 +1,20 @@
-#'  Function that creates a mask layer for an x3p of a bullet land based on the predictions
-#'  of the Hough process. Requires ability to use x3p_image()
+#' Create mask layer for grooves identified with hough
 #'
-#'  @param land.x3p an x3p file containing a bullet land, including surface matrix
-#'  @param grooves a list object containing the output of the get_grooves_hough function. Should include a left and right groove fit equations
-#'  @return an x3p file with the added mask that estimates the GEAs from the Hough method.
+#' Function that creates a mask layer for an x3p of a bullet land based on the predictions
+#' of the Hough process.
 #'
-#'  @importFrom x3ptools x3p_get_scale
-#'  @importFrom x3ptools x3p_add_mask
-#'  @importFrom raster as.matrix
-#'
-#'  @examples
-#'
-#' data("br411", package = "bulletxtrctr")
-#' land.x3p <- br411
-#' grooves <-get_grooves_hough(x3p_to_df(x3p), qu = 0.995)
+#' @param land.x3p an x3p file containing a bullet land, including surface matrix
+#' @param grooves a list object containing the output of the get_grooves_hough function. Should include a left and right groove fit equations
+#' @return an x3p file with the added mask that estimates the GEAs from the Hough method.
+#' @export
+#' @importFrom x3ptools x3p_get_scale x3p_add_mask
+#' @examples
+#' library(x3ptools)
+#' data("br411")
+#' x3p <- br411
+#' grooves <- get_grooves_hough(x3p_to_df(x3p), qu = 0.995)
 #'
 #' a <- get_mask_hough(x3p, grooves)
-#'
 get_mask_hough <- function(land.x3p, grooves){
   left <- grooves$left.groove.fit(0:(ncol(land.x3p$surface.matrix)-1)*x3p_get_scale(land.x3p))
   left <- floor(left/x3p_get_scale(land.x3p) + 1)
@@ -28,7 +26,7 @@ get_mask_hough <- function(land.x3p, grooves){
       x3p_add_mask()
   }
 
-  mask <- raster::as.matrix(land.x3p$mask)
+  mask <- land.x3p$mask
   mask <- sapply(1:length(left), FUN = function(i){
     mask[ i,1:floor(left[i])] <- "#c4221a"
     mask[i,]
