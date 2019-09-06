7538033b4b1d7b59d466169379ace7e639e1aec8
Susan Vanderplas <srvanderplas@gmail.com>
2019-09-06 08:46:13 -0500

    code to generate small x3p file if warnings get too annoying

---
+++ data-raw/make_small_x3p.R
@@ -0,0 +1,8 @@
+# data("br411", package = "bulletxtrctr")
+#
+# x3p <- br411
+# x3p$header.info$sizeY <- 50
+# x3p$surface.matrix <- x3p$surface.matrix[,50:99]
+#
+# small_x3p <- x3ptools::df_to_x3p(x3ptools::x3p_to_df(x3p))
+# usethis::use_data(small_x3p)
