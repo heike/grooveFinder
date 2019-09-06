203acf45cddad64716d737a61708968007e3cd3e
Susan Vanderplas <srvanderplas@gmail.com>
2019-09-06 08:42:22 -0500

    documentation changes, fix params in get_grooves_bcp

--- R/get_grooves_bcp.R
+++ R/get_grooves_bcp.R
@@ -1,9 +1,10 @@
 #' Use Bayesian Changepoint Detection to identify groove locations
 #'
 #' @inheritParams get_grooves_quadratic
+#' @param ... additional arguments to get_grooves_bcp
 #' @importFrom bulletcp get_grooves_bcp
 #' @export
-get_grooves_bcp <- function(x, value, adjust = 10, return_plot = FALSE) {
+get_grooves_bcp <- function(x, value, adjust = 10, return_plot = FALSE, ...) {
   land <- NULL # make R CMD CHECK happy
   bcp_out <- bulletcp::get_grooves_bcp(x = x, value = value, adjust = adjust, ...)
   if (return_plot == T) {
