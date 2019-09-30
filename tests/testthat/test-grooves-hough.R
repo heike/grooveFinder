f5e50b0a13d4a5887692585f0fea4e3ee52f23f3
c-roiger <charlotte.roiger@gmail.com>
2019-09-29 23:15:23 -0500

    adding test files for get_mask_hough

--- tests/testthat/test-grooves-hough.R
+++ tests/testthat/test-grooves-hough.R
@@ -1,12 +1,14 @@
-context("test-grooves-hough")
+context("get_grooves_hough")
 
 data(br411)
 x3p <- br411
+exnames <- c("left.groove.fit", "right.groove.fit")
 
 test_that("get_grooves_hough works", {
-  grooves <- grooveFinder::get_grooves_hough(x3ptools::x3p_to_df(x3p), qu = 0.995)
+  grooves <- grooveFinder::get_grooves_hough(x3ptools::x3p_to_df(x3p))
   expect_is(grooves, "list")
   expect_is(grooves[[1]], "function")
   expect_is(grooves[[2]], "function")
   expect_is(grooves$left.groove.fit(100), "numeric")
+  expect_equivalent(names(grooves), exnames)
 })
